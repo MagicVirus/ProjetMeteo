f43a355888fbb319c3a9b100248c8de2a0026e56
Karupovic Denis <Denis.Karupovic@etu.udamail.fr>
2017-12-12 16:41:15 +0100

    ok

--- src/city/Main.java
+++ src/city/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../window/MeteoMain.fxml"));
         primaryStage.setTitle("Weather Viewer");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
